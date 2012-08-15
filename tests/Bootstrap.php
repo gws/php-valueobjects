462a4b4c60fa4bcfd67b211dc38114622004f0a7
Gordon Stratton <gordon.stratton@gmail.com>
2012-08-15 11:45:33 -0700

    shorter PSR-0 autoloader, shorter root path const

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,38 +1,34 @@
 <?php
 
-// Set appropriate error reporting
 error_reporting(E_ALL | E_STRICT);
 
-// Find the root
-define('TESTS_VO_ROOT', realpath(__DIR__));
+define('VO_ROOT', realpath(__DIR__));
 
 // Override our include path
 set_include_path(
     implode(
         PATH_SEPARATOR,
         array(
-            TESTS_VO_ROOT,
+            VO_ROOT,
             dirname(__DIR__) . DIRECTORY_SEPARATOR . 'library',
             get_include_path()
         )
     )
 );
 
-spl_autoload_register(function($class) {
-    if (class_exists($class, false) || interface_exists($class, false)) {
-        return;
-    }
-
-    $className = ltrim($class, '\\');
-    $file = '';
+// Use small PSR-0 compatible autoloader
+spl_autoload_register(function($className) {
+    $className = ltrim($className, '\\');
+    $fileName  = '';
     $namespace = '';
+
     if ($lastNsPos = strripos($className, '\\')) {
         $namespace = substr($className, 0, $lastNsPos);
         $className = substr($className, $lastNsPos + 1);
-        $file      = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
 
-    $file .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
-    include $file;
+    @include_once $fileName;
 });

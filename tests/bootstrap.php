9bff39e1aa83aa81b02244592ebc2d1359b6922e
Gordon Stratton <gordon.stratton@gmail.com>
2014-01-19 08:52:23 +0000

    Simplify test infrastructure
    * Remove run-tests test runner
    * Assume composer's autoloader is present, do not attempt to fall back
      to include_path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,10 +2,4 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-if (is_readable(__DIR__ . '/../vendor/autoload.php')) {
-    require __DIR__ . '/../vendor/autoload.php';
-} else {
-    set_include_path(
-        dirname(__DIR__) . '/library' . PATH_SEPARATOR . get_include_path()
-    );
-}
+require __DIR__ . '/../vendor/autoload.php';
